fe154b0a1ecfb2737759f3d5b9b9723920df9173
darcy <darcytang@tencent.com>
2023-04-02 21:00:09 +0800

    demodemo

---
+++ apps/admin/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "test/protocol/admin"
+
+	// 注意v2版本
+	"google.golang.org/grpc"
+)
+
+type serverImp struct {
+}
+
+func NewServer() pb.AdminServer {
+	return &serverImp{}
+}
+
+func (s *serverImp) Registration(context.Context, *pb.RegistrationReq) (*pb.RegistrationReq, error) {
+	return nil, nil
+}
+
+func main() {
+	// Create a listener on TCP port
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	// 创建一个gRPC server对象
+	s := grpc.NewServer()
+	// 注册Greeter service到server
+	pb.RegisterAdminServer(s, server{})
+	// 启动gRPC Server
+	log.Println("Serving gRPC on 0.0.0.0:8080")
+	log.Fatal(s.Serve(lis))
+}
